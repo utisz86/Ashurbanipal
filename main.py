e8db8a240fe8f1c60dcefac2c52975bfedbe81cb
Soma <soma.kormanik@outlook.com>
2021-04-04 13:06:29 +0200

    0.2 Basic structure: Add comments

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+# Ashurbanipal
+# This application is a multiple platform username/password storage,
+# which syncronasa the data through an email client.
+
+# Kivy moduls
 import kivy
-kivy.require('2.0.0') # replace with your current kivy version !
+kivy.require('2.0.0')
 
 from kivy.app import App
 from kivy.uix.label import Label
+
+# Own modul for data
 
 
 class AshurbanipalApp(App):
@@ -10,7 +17,8 @@ class AshurbanipalApp(App):
     def build(self):
         return Label(text='Hello world')
 
+# Start the Kivy Application loophole
+
 
 if __name__ == '__main__':
     AshurbanipalApp().run()
-
